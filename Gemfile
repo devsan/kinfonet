d8d90d4989ec2d9fdb924dbe6065b091beeddece
madebydna <info@madebydna.com>
2013-08-15 14:20:32 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+#ruby-gemset=kinfonet4
+
+gem 'rails', '4.0.0'
+
+gem 'pg'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+
+# Use CoffeeScript for .js.coffee assets and views
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :production do
+  gem 'rails_12factor', '~> 0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kinfonet4::Application.config.secret_key_base = 'cd95150aa29e377f61d94d42f01aa3c882bd9ceadfc77eb2f5abb83c75afd4bb040b3697ee26da805e3803b8d5aae042b389fa6d6459fd09832d6c0bde9ea3c4'
